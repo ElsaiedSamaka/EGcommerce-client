66d6459f9b9d955e2ff5f2e9df304a12321e30b4
unknown <elsaied.samaka1997@gmail.com>
2022-02-11 18:54:32 +0200

    setup _document.js

---
+++ client/pages/_document.js
@@ -0,0 +1,48 @@
+import Document, { Head, Html, Main, NextScript } from 'next/document'
+import React from 'react'
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx)
+    return { ...initialProps }
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head>
+          {/* base */}
+          <meta charSet="utf-8" />
+          <meta httpEquiv="x-ua-compatible" content="ie=edge" />
+          <meta name="description" content="best ecommerce website" />
+
+          {/* favicon */}
+                <link rel="shortcut icon" href="/images/Saied_Samaka.png" />
+                
+                		
+		{/* <!-- fonts files --> */}
+		<link href='http://fonts.googleapis.com/css?family=Karla:400,400italic,700,700italic' rel='stylesheet' type='text/css'/>
+                <link href='http://fonts.googleapis.com/css?family=Libre+Baskerville:400,400italic,700' rel='stylesheet' type='text/css' />
+                
+        {/* <!-- Font awesome css --> */}
+		<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css"/>
+		{/* <!-- Revolution slider css --> */}
+		<link href="js/rs-plugin/css/settings.css" rel="stylesheet" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
+
+export default MyDocument
+    
+{/* <script
+                type="text/javascript"
+                dangerouslySetInnerHTML={{
+                  __html: `window.dataLayer = window.dataLayer || [];function gtag(){dataLayer.push(arguments);}gtag('js', new Date());gtag('config', '${SiteConfig.googleAnalytic}');`
+                }}
+              /> */}
