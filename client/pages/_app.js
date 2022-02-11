d3646f7a2087136a21d88943f18ea075e835c0d5
unknown <elsaied.samaka1997@gmail.com>
2022-02-11 17:23:14 +0200

    FstC - setup F.E.

---
+++ client/pages/_app.js
@@ -0,0 +1,20 @@
+import "../styles/css/bootstrap-select.min.css";
+import "../styles/css/bootstrap.css";
+import "../styles/css/glasscase.css";
+import "../styles/css/glasscase.minf195.css";
+import "../styles/css/hover-effect.css";
+import "../styles/css/owl.carousel.css";
+import "../styles/css/responsive.css";
+import "../styles/style.css";
+
+
+
+
+
+
+
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />;
+}
+
+export default MyApp;
