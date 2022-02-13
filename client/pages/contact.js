31d41445e4a8ac83e2c9030929d5a55b67334306
unknown <elsaied.samaka1997@gmail.com>
2022-02-13 11:23:55 +0200

    working Proj !

--- client/pages/contact.js
+++ client/pages/contact.js
@@ -1,23 +1,24 @@
 import AddressDetail from "../components/address/AddressDetail";
-import ContactForm from "../components/contactForm/ContactForm";
+// import ContactForm from "../components/contactForm/ContactForm";
 import Footer from "../components/footer/Footer";
 import Header from "../components/header/Header";
 import NavBar from "../components/header/NavBar";
 import BreadCumb from "../components/util/BreadCumb";
 import InfoRow from "../components/util/InfoRow";
 
-
 export default function contact() {
-    return <>
-        <Header />
-        <NavBar />
-        <div className="contact_map_area">
-            <div id="googleMap" style={{ width: '100 %', height: '345px' }}></div>
-        </div>
-       <InfoRow />
-        <BreadCumb />
-        <AddressDetail />
-        <ContactForm />
-        <Footer />
+  return (
+    <>
+      <Header />
+      <NavBar />
+      <div className='contact_map_area'>
+        <div id='googleMap' style={{ width: "100 %", height: "345px" }}></div>
+      </div>
+      <InfoRow />
+      <BreadCumb />
+      <AddressDetail />
+      {/* <ContactForm /> */}
+      <Footer />
     </>
-};
+  );
+}
