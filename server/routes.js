b13da5e06b4f93819f0853aaadbca12d8cb79787
unknown <elsaied.samaka1997@gmail.com>
2022-02-15 23:55:42 +0200

    setup a route all products ../api/

--- server/routes.js
+++ server/routes.js
@@ -1,6 +1,17 @@
-const router = require("express").Router();
-router.get("/", (req, res) => {
-  res.json({ mssg: "Hello" });
+const Product = require( './models/product' )
+const Category = require('./models/category')
+
+const router = require( "express" ).Router();
+router.get("/",async (req, res) => {
+    try {
+    const products = await Product.find({})
+      .sort("-createdAt")
+      .populate("category");
+      res.send( products );
+  } catch (error) {
+    console.log(error);
+    res.redirect("/");
+  }
 });
 module.exports = (app) => {
   app.use("/api", router);
