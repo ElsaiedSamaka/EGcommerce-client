d91768a609a70c977fe751486650daa5cf6b39f3
Elsaied samaka <elsaied.samaka1997@gmail.com>
2022-11-01 13:34:06 +0200

    wrap front-end up

--- client/components/home_slider/HomeSlider.jsx
+++ client/components/home_slider/HomeSlider.jsx
@@ -1,34 +1,93 @@
+import dynamic from "next/dynamic";
+import "owl.carousel/dist/assets/owl.carousel.css";
+import "owl.carousel/dist/assets/owl.theme.default.css";
+var $ = require("jquery");
+if (typeof window !== "undefined") {
+  window.$ = window.jQuery = require("jquery");
+}
+const OwlCarousel = dynamic(() => import("react-owl-carousel"), {
+  ssr: false,
+});
 export default function HomeSlider({ products }) {
+  console.log(products[1].category);
+  const options = {
+    margin: 30,
+    responsiveClass: true,
+    nav: true,
+    dots: true,
+    autoplay: false,
+    smartSpeed: 1000,
+    navClass: ["owl-prev", "owl-next"],
+    navText: ["", ""],
+    responsive: {
+      0: {
+        items: 1,
+      },
+      400: {
+        items: 1,
+      },
+      600: {
+        items: 2,
+      },
+      700: {
+        items: 3,
+      },
+      1000: {
+        items: 4,
+      },
+    },
+  };
+
   return (
     <div className="home3_slider_area">
-      <div className="item ">
-        <div className="home3_slider_image">
-          <div className="slider3-img cc-h-layer">
-            <img src="/images/homepage2_s4.jpg" alt="" />
-            <div className="b_hover_box">
-              <h4>ROYAL COLLECTIONS</h4>
-              <p>Lorem ipsum dolor sit amet, consectetur</p>
-            </div>
-          </div>
-          <div className="home3_slider_text">
-            <div className="home3_slider_head">
-              <a href="">
-                <h2>
-                  <span>ROYAL</span>
-                  <br />
-                  COLLECTIONS
-                </h2>
-              </a>
-            </div>
-            <div className="home3_slider_bottom_left">
-              <a href="">GO TO COLLECTION</a>
-            </div>
-            <div className="home3_slider_bottom_right">
-              <a href="">OTHER CLOTHERS</a>
-            </div>
-          </div>
-        </div>
-      </div>
+      <OwlCarousel
+        className="owl-theme"
+        loop
+        margin={4}
+        nav={true}
+        navText={["left", "right"]}
+        dots={false}
+        animateIn={true}
+        {...options}
+      >
+        {products && products.length > 0
+          ? products.map((product, i) => (
+              <div key={i} className="item ">
+                <div className="home3_slider_image">
+                  <div className="slider3-img cc-h-layer">
+                    {product.image ? (
+                      <img src={product.image} alt={product.title} />
+                    ) : (
+                      <img src="/images/homepage2_s4.jpg" alt={product.title} />
+                    )}
+
+                    <div className="b_hover_box">
+                      <h4>{product.category}</h4>
+                      <p>Lorem ipsum dolor sit amet, consectetur</p>
+                    </div>
+                  </div>
+                  <div className="home3_slider_text">
+                    <div className="home3_slider_head">
+                      <a href="">
+                        <h2>
+                          <span>{product.category}</span>
+                          <br />
+                          COLLECTIONS
+                        </h2>
+                      </a>
+                    </div>
+                    <div className="home3_slider_bottom_left">
+                      <a href="">GO TO COLLECTION</a>
+                    </div>
+                    <div className="home3_slider_bottom_right">
+                      <a href="">OTHER CLOTHERS</a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            ))
+          : "loading"}
+      </OwlCarousel>
     </div>
   );
 }
--- client/components/testimonial/Testimonial.jsx
+++ client/components/testimonial/Testimonial.jsx
@@ -1,102 +1,168 @@
 export default function Testimonial() {
-    return 	<div className="home_testimonial_area">
-			<div className="container">
-				<div className="row">
-					<div className="col-md-12 col-sm-12 col-xs-12">
-						
-						<div id="carousel-example-generic" className="carousel slide" data-ride="carousel">
-						
-						  {/* <!-- Indicators --> */}
-						  <ol className="carousel-indicators">
-							<li data-target="#carousel-example-generic" data-slide-to="0" className="active"></li>
-							<li data-target="#carousel-example-generic" data-slide-to="1"></li>
-							<li data-target="#carousel-example-generic" data-slide-to="2"></li>
-						  </ol>
-						  
-						  {/* <!-- Wrapper for slides --> */}
-						  <div className="carousel-inner" role="listbox">
-							<div className="item active">
-								<div className="home_testimonial">
-									<div className="home_testimonial_img">
-										<img src="images/caro_h_quote.png" alt="quote" />
-									</div>
-									<div className="home_testimonial_text">
-										<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin in tortor eget nulla elementum mollis augue.<br/> 
-										Maecenas eu hendrerit leo. Nunc quam tortor, mollis at tempor ut, pharetra ut dolor. Maecenas porta,<br/> 
-										lacus eu efficitur tincidunt, nisl erat iaculis nisi, nec efficitur ante quam non tellus.</p>
-									</div>
-									<div className="small_line"></div>
-									<div className="home_testimonial_author">
-										<span>JOHNATHON DOE</span>
-										<div className="testi_stars">
-											<ul>
-												<li><a className="fa fa-star" ></a></li>
-												<li><a className="fa fa-star" ></a></li>
-												<li><a className="fa fa-star" ></a></li>
-												<li><a className="fa fa-star" ></a></li>
-												<li><a className="fa fa-star-o" ></a></li>
-											</ul>
-										</div>
-									</div>
-								</div>
-							</div>
-							<div className="item">
-								<div className="home_testimonial">
-									<div className="home_testimonial_img">
-										<img src="images/caro_h_quote.png" alt="quote" />
-									</div>
-									<div className="home_testimonial_text">
-										<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin in tortor eget nulla elementum mollis augue.<br/> 
-										Maecenas eu hendrerit leo. Nunc quam tortor, mollis at tempor ut, pharetra ut dolor. Maecenas porta,<br/> 
-										lacus eu efficitur tincidunt, nisl erat iaculis nisi, nec efficitur ante quam non tellus.</p>
-									</div>
-									<div className="small_line"></div>
-									<div className="home_testimonial_author">
-										<span>JOHNATHON DOE</span>
-										<div className="testi_stars">
-											<ul>
-												<li><a className="fa fa-star" ></a></li>
-												<li><a className="fa fa-star" ></a></li>
-												<li><a className="fa fa-star" ></a></li>
-												<li><a className="fa fa-star" ></a></li>
-												<li><a className="fa fa-star-o" ></a></li>
-											</ul>
-										</div>
-									</div>
-								</div>
-							</div>
-							<div className="item">
-								<div className="home_testimonial">
-									<div className="home_testimonial_img">
-										<img src="images/caro_h_quote.png" alt="quote" />
-									</div>
-									<div className="home_testimonial_text">
-										<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin in tortor eget nulla elementum mollis augue.<br/> 
-										Maecenas eu hendrerit leo. Nunc quam tortor, mollis at tempor ut, pharetra ut dolor. Maecenas porta,<br/> 
-										lacus eu efficitur tincidunt, nisl erat iaculis nisi, nec efficitur ante quam non tellus.</p>
-									</div>
-									<div className="small_line"></div>
-									<div className="home_testimonial_author">
-										<span>JOHNATHON DOE</span>
-										<div className="testi_stars">
-											<ul>
-												<li><a className="fa fa-star" ></a></li>
-												<li><a className="fa fa-star" ></a></li>
-												<li><a className="fa fa-star" ></a></li>
-												<li><a className="fa fa-star" ></a></li>
-												<li><a className="fa fa-star-o" ></a></li>
-											</ul>
-										</div>
-									</div>
-								</div>
-							</div>
-						  </div>
+    return (
+      <div className="home_testimonial_area">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-12 col-sm-12 col-xs-12">
+              <div
+                id="carousel-example-generic"
+                className="carousel slide"
+                data-ride="carousel"
+              >
+                {/* <!-- Indicators --> */}
+                <ol className="carousel-indicators">
+                  <li
+                    data-target="#carousel-example-generic"
+                    data-slide-to="0"
+                    className="active"
+                  ></li>
+                  <li
+                    data-target="#carousel-example-generic"
+                    data-slide-to="1"
+                  ></li>
+                  <li
+                    data-target="#carousel-example-generic"
+                    data-slide-to="2"
+                  ></li>
+                </ol>
 
-						</div>
-						
-					</div>
-				</div>
-			</div>
+                {/* <!-- Wrapper for slides --> */}
+                <div className="carousel-inner" role="listbox">
+                  <div className="item active">
+                    <div className="home_testimonial">
+                      <div className="home_testimonial_img">
+                        <img src="/images/caro_h_quote.png" alt="quote" />
+                      </div>
+                      <div className="home_testimonial_text">
+                        <p>
+                          Lorem ipsum dolor sit amet, consectetur adipiscing
+                          elit. Proin in tortor eget nulla elementum mollis
+                          augue.
+                          <br />
+                          Maecenas eu hendrerit leo. Nunc quam tortor, mollis at
+                          tempor ut, pharetra ut dolor. Maecenas porta,
+                          <br />
+                          lacus eu efficitur tincidunt, nisl erat iaculis nisi,
+                          nec efficitur ante quam non tellus.
+                        </p>
+                      </div>
+                      <div className="small_line"></div>
+                      <div className="home_testimonial_author">
+                        <span>JOHNATHON DOE</span>
+                        <div className="testi_stars">
+                          <ul>
+                            <li>
+                              <a className="fa fa-star"></a>
+                            </li>
+                            <li>
+                              <a className="fa fa-star"></a>
+                            </li>
+                            <li>
+                              <a className="fa fa-star"></a>
+                            </li>
+                            <li>
+                              <a className="fa fa-star"></a>
+                            </li>
+                            <li>
+                              <a className="fa fa-star-o"></a>
+                            </li>
+                          </ul>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="item">
+                    <div className="home_testimonial">
+                      <div className="home_testimonial_img">
+                        <img src="images/caro_h_quote.png" alt="quote" />
+                      </div>
+                      <div className="home_testimonial_text">
+                        <p>
+                          Lorem ipsum dolor sit amet, consectetur adipiscing
+                          elit. Proin in tortor eget nulla elementum mollis
+                          augue.
+                          <br />
+                          Maecenas eu hendrerit leo. Nunc quam tortor, mollis at
+                          tempor ut, pharetra ut dolor. Maecenas porta,
+                          <br />
+                          lacus eu efficitur tincidunt, nisl erat iaculis nisi,
+                          nec efficitur ante quam non tellus.
+                        </p>
+                      </div>
+                      <div className="small_line"></div>
+                      <div className="home_testimonial_author">
+                        <span>JOHNATHON DOE</span>
+                        <div className="testi_stars">
+                          <ul>
+                            <li>
+                              <a className="fa fa-star"></a>
+                            </li>
+                            <li>
+                              <a className="fa fa-star"></a>
+                            </li>
+                            <li>
+                              <a className="fa fa-star"></a>
+                            </li>
+                            <li>
+                              <a className="fa fa-star"></a>
+                            </li>
+                            <li>
+                              <a className="fa fa-star-o"></a>
+                            </li>
+                          </ul>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="item">
+                    <div className="home_testimonial">
+                      <div className="home_testimonial_img">
+                        <img src="images/caro_h_quote.png" alt="quote" />
+                      </div>
+                      <div className="home_testimonial_text">
+                        <p>
+                          Lorem ipsum dolor sit amet, consectetur adipiscing
+                          elit. Proin in tortor eget nulla elementum mollis
+                          augue.
+                          <br />
+                          Maecenas eu hendrerit leo. Nunc quam tortor, mollis at
+                          tempor ut, pharetra ut dolor. Maecenas porta,
+                          <br />
+                          lacus eu efficitur tincidunt, nisl erat iaculis nisi,
+                          nec efficitur ante quam non tellus.
+                        </p>
+                      </div>
+                      <div className="small_line"></div>
+                      <div className="home_testimonial_author">
+                        <span>JOHNATHON DOE</span>
+                        <div className="testi_stars">
+                          <ul>
+                            <li>
+                              <a className="fa fa-star"></a>
+                            </li>
+                            <li>
+                              <a className="fa fa-star"></a>
+                            </li>
+                            <li>
+                              <a className="fa fa-star"></a>
+                            </li>
+                            <li>
+                              <a className="fa fa-star"></a>
+                            </li>
+                            <li>
+                              <a className="fa fa-star-o"></a>
+                            </li>
+                          </ul>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
+        </div>
+      </div>
+    );
 
 };
